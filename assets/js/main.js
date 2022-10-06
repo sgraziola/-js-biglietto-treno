92dc393e3cb9c6a54ea7cc72d686dbd87f74a8f2
sgraziola <simo.fdl@gmail.com>
2022-10-06 16:22:16 +0200

    Ex Ticket Train: fix "Let" abuse

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,23 +10,18 @@ const userAge = Number(prompt("Quanti anni hai?"));
 const eurosPerKm = 0.21;
 const travelFullPrice = travelLenght * eurosPerKm;
 //console.log(travelFullPrice);
-let finalPrice;
+//prezzo pieno
+let finalPrice = travelFullPrice;
 const juniorDiscount = 20 / 100;
 const seniorDiscount = 40 / 100;
 if (userAge < 18) {
     //Aggiungere uno sconto del 20% per i minorenni
-    let finalPrice = travelFullPrice - (travelFullPrice * juniorDiscount);  
+    finalPrice = travelFullPrice - (travelFullPrice * juniorDiscount);  
     //console.log(finalPrice);  
-    document.getElementById("ticket_price").innerHTML = (`Il prezzo del tuo biglietto è ${finalPrice.toFixed(2)} €`);
 } else if (userAge >= 65) {
     //Aggiungere uno sconto del 40% per gli over 65.
-    let finalPrice = travelFullPrice - (travelFullPrice * seniorDiscount);
+    finalPrice = travelFullPrice - (travelFullPrice * seniorDiscount);
     //console.log(finalPrice);
-    document.getElementById("ticket_price").innerHTML = (`Il prezzo del tuo biglietto è ${finalPrice.toFixed(2)} €`);
-} else {
-    //Prezzo pieno
-    let finalPrice = travelFullPrice;
-    //console.log(finalPrice);
-    document.getElementById("ticket_price").innerHTML = (`Il prezzo del tuo biglietto è ${finalPrice.toFixed(2)} €`);
-}
+} 
 //L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
+document.getElementById("ticket_price").innerHTML = `Il prezzo del tuo biglietto è ${finalPrice.toFixed(2)} €`;
